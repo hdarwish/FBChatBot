e7e008880909532b9cf8dee15c12859801f55707
hafs <hafs.darwish@vodafone.com>
2018-12-14 13:41:19 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict'
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const request = require('request');
+const path = require('path');
+
+app.use(bodyparser.urlencoded({extended : false}));
+app.use(bodyparser.json());
+app.use(express.static(path.join(__dirname,'./public')));
+
+app.get('/', (req,res)=>{
+  res.send('HI i am a chatbot');
+});
+app.get('/webhook', (req,res)=>{
+    if(req.query['hub.verify_token'] === 'HafsBytes'){
+      res.send(req.query['hub.challenge']);
+    }
+    res.send('wrong token');
+
+});
+app.post('/webhook', (req,res)=>{
+  let messaging_events = req.body.entry[0].messaging_events;
+  for(let i = 0; i<messaging_events.length;i++){
+    let event = messaging_events[i];
+    let sender = event.sender.id;
+    if(event.message && event.message.text){
+      let text = event.message.text;
+      sendText(sender,"Text echo: "+ text.substring(0,100));
+    }
+  }
+  res.sendStatus(200);
+});
+let token = "EAAMaw038IjsBAJII9gOvZCqwO9KYZAZCvYl3vOZCeOdQaTN7JsxvgAQQkbKsvqpXd0Kgtm5eaHroDgiIWhZCO7VnORuMIZAQZCzIArEl8nLJxvGaWfqDe9ZAoG0GeV92yYKRTTuUcZBZARJ2KBjbZA3ZAPZBc4QBSxvkL2TkKNCwnEGCxLMkSHdP6dpOS"
+function sendText(sender, text){
+  let messageData = {text:text};
+  request({
+    url:"https://graph.facebook.com/v2.6/me/messages",
+    qs: {access_token, token},
+    method: "POST",
+    json:{
+      receipt:{id:sender},
+      message: messageData
+    }
+  },function(error, response, body){
+    if(error){
+      console.log("sending error");
+    }else if(response.body.error){
+      console.log("response body error");
+    }
+  });
+}
+
+const port = process.env.PORT || '5000';
+app.set('port', port);
+
+app.listen(port, () => console.log(`Running on localhost:${port}`));
